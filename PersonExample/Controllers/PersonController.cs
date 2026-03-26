623be41631f6b93400bb176ca921ba19b0903c77
Oliver Zott <zott_oliver@web.de>
2026-03-26 22:38:40 +0100

    feat: add simple person get endpoint and sqitch to file-scoped namespaces

---
+++ PersonExample/Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonExample.Entities;
+
+namespace PersonExample.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PersonController : ControllerBase
+{
+
+    [HttpGet]
+    public IEnumerable<Person> Get()
+    {
+        return
+        [
+            new() { Id = 1, FirstName = "John", LastName = "Doe", Age = 30 },
+            new() { Id = 2, FirstName = "Jane", LastName = "Smith", Age = 25 },
+            new() { Id = 3, FirstName = "Bob", LastName = "Johnson", Age = 40 }
+        ];
+    }
+
+}
---
+++ PersonExample/Entities/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonExample.Entities;
+
+
+public class Person
+{
+
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string FirstName { get; set; } = "";
+
+    [Required]
+    [MaxLength(50)]
+    public string LastName { get; set; } = "";
+
+    [Range(0, 120)]
+    public int Age { get; set; }
+
+}
